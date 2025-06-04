de9a52b8a236c9732f06b080ddf08ccdea07a328
brighton404 <76064642+brighton404@users.noreply.github.com>
2025-06-04 20:05:01 +0300

    Defined the product form

--- src/renderer/components/productForm.tsx
+++ src/renderer/components/productForm.tsx
@@ -12,12 +12,16 @@ const ProductForm = ({ onAdd }: { onAdd: () => void }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} style={{ marginBottom: '1rem' }}>
-      <input placeholder="Name" value={product.name}
+    <form onSubmit={handleSubmit} style={{ marginBottom: '1rem' }} id='prdForm'>
+      <h2>Add a product</h2>
+      <label htmlFor="productName">Name</label>
+      <input placeholder="Name" value={product.name} aria-label='productName'
         onChange={(e) => setProduct({ ...product, name: e.target.value })} />
-      <input type="number" placeholder="Price" value={product.price}
+      <label htmlFor="productPrice">Price</label>
+      <input type="number" placeholder="Price" value={product.price} aria-label='productPrice'
         onChange={(e) => setProduct({ ...product, price: parseFloat(e.target.value) })} />
-      <input type="number" placeholder="Stock" value={product.stock}
+      <label htmlFor="productStock">Stock</label>
+      <input type="number" placeholder="Stock" value={product.stock} aria-label='productStock'
         onChange={(e) => setProduct({ ...product, stock: parseInt(e.target.value) })} />
       <button type="submit">Add Product</button>
     </form>
