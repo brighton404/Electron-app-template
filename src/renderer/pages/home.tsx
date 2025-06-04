f8119e592398da27624b89cc78d79dad20092df9
brighton404 <76064642+brighton404@users.noreply.github.com>
2025-06-04 20:00:59 +0300

    separated the product page

--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -1,10 +1,7 @@
-import ProductPage from '../components/productPage';
-
 const Home = () => {
   return (
     <section className='page'>
       <h1>Home Page</h1>
-      <ProductPage />
     </section>
   )
 }
