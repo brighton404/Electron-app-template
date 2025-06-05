88dadb5477a0dc171ff1297d0b7147cd5421d8ac
brighton404 <76064642+brighton404@users.noreply.github.com>
2025-06-05 17:36:40 +0300

    modified resize

--- src/renderer/pages/products.tsx
+++ src/renderer/pages/products.tsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import ProductForm from '@/renderer/components/productForm';
 import ProductTable from '@/renderer/components/productList';
-import ResizeLayout from '../components/resize';
+import Resizable from '../components/resize';
 
 const ProductPage = () => {
   const [refresh, setRefresh] = useState(false);
   return (
-    <section className='page product'>
-      <ResizeLayout>
-        <ProductTable key={refresh.toString()} />
-        <ProductForm onAdd={() => setRefresh(!refresh)} />
-      </ResizeLayout>
-    </section>
+      <section className='page product'>
+        <div className="extend">
+        <section className='content'>
+          <ProductTable key={refresh.toString()} />
+        </section>
+        </div>
+        <Resizable side="left">
+          <ProductForm onAdd={() => setRefresh(!refresh)} />
+        </Resizable>
+      </section>
   );
 };
 
