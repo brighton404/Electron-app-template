9f49fdb7d1ce1d050e6a0b61a05b98e8e4857d37
brighton404 <76064642+brighton404@users.noreply.github.com>
2025-06-05 17:39:43 +0300

    header

--- src/renderer/components/header.tsx
+++ src/renderer/components/header.tsx
@@ -1,26 +1,26 @@
-import { X, Monitor, Minus } from 'lucide-react';
+import { X, Monitor, Minus, Grip } from 'lucide-react';
 import Tooltip from './tooltip';
 
 const Header = () => {
     return (
-    <header style={{ WebkitAppRegion: 'drag'} as any}>
-    <div className="win-Buttons" style={{ display: 'flex', gap: '0px', WebkitAppRegion: 'no-drag' }as any}>
-        <Tooltip text="minimize" position='bottom'>
-            <button onClick={() => window.electronAPI.minimize()}>
-            <Minus color="gray" size={18} />
-            </button>
-        </Tooltip>
-        <Tooltip text="maximize" position='bottom'>
-            <button onClick={() => window.electronAPI.maximize()}>
-                <Monitor color="gray" size={16} />
-            </button>
-        </Tooltip>
-        <Tooltip text="Close" position='bottom'>
-            <button onClick={() => window.electronAPI.close()} className='critical'>
-                <X color="gray" size={18} />
-            </button>
-        </Tooltip>
-    </div>
+    <header style={{ display: 'flex', gap: '0px', WebkitAppRegion: 'drag' }as any}>
+        <div className="win-Buttons" style={{ display: 'flex', gap: '0px', WebkitAppRegion: 'no-drag' }as any}>
+            <Tooltip text="minimize" position='bottom'>
+                <button onClick={() => window.electronAPI.minimize()}>
+                <Minus color="gray" size={18} />
+                </button>
+            </Tooltip>
+            <Tooltip text="maximize" position='bottom'>
+                <button onClick={() => window.electronAPI.maximize()}>
+                    <Monitor color="gray" size={16} />
+                </button>
+            </Tooltip>
+            <Tooltip text="Close" position='bottom'>
+                <button onClick={() => window.electronAPI.close()} className='critical'>
+                    <X color="gray" size={18} />
+                </button>
+            </Tooltip>
+        </div>
     </header>
     )
 }
